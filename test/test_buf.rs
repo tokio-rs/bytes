6e2fd1a324d6844510c46944851d5355d9dd814f
Florian Hartwig <florian.j.hartwig@gmail.com>
2015-09-16 21:12:32 +0200

    Fix test_vec_as_mut_buf

--- test/test_buf.rs
+++ test/test_buf.rs
@@ -32,7 +32,7 @@ pub fn test_vec_as_mut_buf() {
     assert_eq!(buf.remaining(), usize::MAX);
 
     unsafe {
-        assert_eq!(buf.mut_bytes().len(), 64);
+        assert_eq!(buf.mut_bytes().len(), 128);
     }
 
     buf.write(&b"zomg"[..]).unwrap();
@@ -40,12 +40,12 @@ pub fn test_vec_as_mut_buf() {
     assert_eq!(&buf, b"zomg");
 
     assert_eq!(buf.remaining(), usize::MAX - 4);
-    assert_eq!(buf.capacity(), 64);
+    assert_eq!(buf.capacity(), 128);
 
-    for _ in 0..16 {
+    for _ in 0..32 {
         buf.write(&b"zomg"[..]).unwrap();
     }
 
-    assert_eq!(buf.len(), 68);
-    assert_eq!(buf.capacity(), 128);
+    assert_eq!(buf.len(), 132);
+    assert_eq!(buf.capacity(), 384);
 }
