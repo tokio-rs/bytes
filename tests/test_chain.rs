9a4018e7571538c8cabb1cae0bde3f0d8949fde2
Carl Lerche <me@carllerche.com>
2017-03-19 09:08:37 -0700

    Fix tests on nightly
    Closes #83

--- tests/test_chain.rs
+++ tests/test_chain.rs
@@ -53,41 +53,50 @@ fn vectored_read() {
     let b = Cursor::new(Bytes::from(&b"world"[..]));
 
     let mut buf = a.chain(b);
-    let mut iovecs: [&IoVec; 4] = Default::default();
 
-    assert_eq!(2, buf.bytes_vec(&mut iovecs));
-    assert_eq!(iovecs[0][..], b"hello"[..]);
-    assert_eq!(iovecs[1][..], b"world"[..]);
-    assert!(iovecs[2].is_empty());
-    assert!(iovecs[3].is_empty());
+    {
+        let mut iovecs: [&IoVec; 4] = Default::default();
+
+        assert_eq!(2, buf.bytes_vec(&mut iovecs));
+        assert_eq!(iovecs[0][..], b"hello"[..]);
+        assert_eq!(iovecs[1][..], b"world"[..]);
+        assert!(iovecs[2].is_empty());
+        assert!(iovecs[3].is_empty());
+    }
 
     buf.advance(2);
 
-    iovecs = Default::default();
+    {
+        let mut iovecs: [&IoVec; 4] = Default::default();
 
-    assert_eq!(2, buf.bytes_vec(&mut iovecs));
-    assert_eq!(iovecs[0][..], b"llo"[..]);
-    assert_eq!(iovecs[1][..], b"world"[..]);
-    assert!(iovecs[2].is_empty());
-    assert!(iovecs[3].is_empty());
-
-    buf.advance(3);
-
-    iovecs = Default::default();
-
-    assert_eq!(1, buf.bytes_vec(&mut iovecs));
-    assert_eq!(iovecs[0][..], b"world"[..]);
-    assert!(iovecs[1].is_empty());
-    assert!(iovecs[2].is_empty());
-    assert!(iovecs[3].is_empty());
+        assert_eq!(2, buf.bytes_vec(&mut iovecs));
+        assert_eq!(iovecs[0][..], b"llo"[..]);
+        assert_eq!(iovecs[1][..], b"world"[..]);
+        assert!(iovecs[2].is_empty());
+        assert!(iovecs[3].is_empty());
+    }
 
     buf.advance(3);
 
-    iovecs = Default::default();
+    {
+        let mut iovecs: [&IoVec; 4] = Default::default();
 
-    assert_eq!(1, buf.bytes_vec(&mut iovecs));
-    assert_eq!(iovecs[0][..], b"ld"[..]);
-    assert!(iovecs[1].is_empty());
-    assert!(iovecs[2].is_empty());
-    assert!(iovecs[3].is_empty());
+        assert_eq!(1, buf.bytes_vec(&mut iovecs));
+        assert_eq!(iovecs[0][..], b"world"[..]);
+        assert!(iovecs[1].is_empty());
+        assert!(iovecs[2].is_empty());
+        assert!(iovecs[3].is_empty());
+    }
+
+    buf.advance(3);
+
+    {
+        let mut iovecs: [&IoVec; 4] = Default::default();
+
+        assert_eq!(1, buf.bytes_vec(&mut iovecs));
+        assert_eq!(iovecs[0][..], b"ld"[..]);
+        assert!(iovecs[1].is_empty());
+        assert!(iovecs[2].is_empty());
+        assert!(iovecs[3].is_empty());
+    }
 }
