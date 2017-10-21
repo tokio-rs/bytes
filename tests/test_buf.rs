149922d7cf4b76d00066bb8615df2f7b1d9ace44
Carl Lerche <me@carllerche.com>
2017-10-21 15:47:45 -0700

    Get test passing again

--- tests/test_buf.rs
+++ tests/test_buf.rs
@@ -47,7 +47,12 @@ fn test_get_u16_buffer_underflow() {
 #[test]
 fn test_bufs_vec() {
     let buf = Cursor::new(b"hello world");
-    let mut dst: [&IoVec; 2] = Default::default();
+
+    let b1: &[u8] = &mut [0];
+    let b2: &[u8] = &mut [0];
+
+    let mut dst: [&IoVec; 2] =
+        [b1.into(), b2.into()];
 
     assert_eq!(1, buf.bytes_vec(&mut dst[..]));
 }
