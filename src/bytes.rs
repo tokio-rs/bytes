1a7cc1eba3a0b1fc802ad41364d8e8ce1ce66d58
Alan Somers <asomers@gmail.com>
2018-01-14 16:23:13 -0700

    Don't define with_capacity_stable.
    Just document how from(Vec<u8>) can be used to disable inlining.

--- src/bytes.rs
+++ src/bytes.rs
@@ -95,9 +95,10 @@ use std::iter::{FromIterator, Iterator};
 /// # Inline bytes
 ///
 /// As an optimization, when the slice referenced by a `Bytes` or `BytesMut`
-/// handle is small enough [1], `Bytes` will avoid the allocation by inlining
-/// the slice directly in the handle. In this case, a clone is no longer
-/// "shallow" and the data will be copied.
+/// handle is small enough [1], `with_capacity` will avoid the allocation
+/// by inlining the slice directly in the handle. In this case, a clone is no
+/// longer "shallow" and the data will be copied.  Converting from a `Vec` will
+/// never use inlining.
 ///
 /// [1] Small enough: 31 bytes on 64 bit systems, 15 on 32 bit systems.
 ///
@@ -417,27 +418,6 @@ impl Bytes {
         }
     }
 
-    /// Creates a new `Bytes` with the specified capacity, using stable storage
-    ///
-    /// Exactly like `with_capacity`, but never uses the inline optimization.
-    /// The resulting `Bytes` can be used with asynchronous C APIs that require
-    /// stable storage.
-    ///
-    /// # Examples
-    /// ```
-    /// use bytes::Bytes;
-    ///
-    /// let mut bytes = Bytes::with_capacity_stable(4);
-    ///
-    /// assert!(!bytes.is_inline());
-    /// ```
-    #[inline]
-    pub fn with_capacity_stable(capacity: usize) -> Bytes {
-        Bytes {
-            inner: Inner::with_capacity_stable(capacity),
-        }
-    }
-
     /// Creates a new empty `Bytes`.
     ///
     /// This will not allocate and the returned `Bytes` handle will be empty.
@@ -510,6 +490,7 @@ impl Bytes {
     /// use bytes::Bytes;
     ///
     /// assert!(Bytes::with_capacity(4).is_inline());
+    /// assert!(!Bytes::from(Vec::with_capacity(4)).is_inline());
     /// assert!(!Bytes::with_capacity(1024).is_inline());
     /// ```
     pub fn is_inline(&self) -> bool {
@@ -872,6 +853,11 @@ impl From<BytesMut> for Bytes {
 }
 
 impl From<Vec<u8>> for Bytes {
+    /// Convert a `Vec` into a `Bytes`
+    ///
+    /// This constructor may be used to avoid the inlining optimization used by
+    /// `with_capacity`.  A `Bytes` constructed this way will always store its
+    /// data on the heap.
     fn from(src: Vec<u8>) -> Bytes {
         BytesMut::from(src).freeze()
     }
@@ -1050,28 +1036,6 @@ impl BytesMut {
         }
     }
 
-    /// Creates a new `BytesMut` with the specified capacity, using stable
-    /// storage
-    ///
-    /// Exactly like `with_capacity`, but never uses the inline optimization.
-    /// The resulting `BytesMut` can be used with asynchronous C APIs that
-    /// require stable storage.
-    ///
-    /// # Examples
-    /// ```
-    /// use bytes::BytesMut;
-    ///
-    /// let mut bytes = BytesMut::with_capacity_stable(4);
-    ///
-    /// assert!(!bytes.is_inline());
-    /// ```
-    #[inline]
-    pub fn with_capacity_stable(capacity: usize) -> BytesMut {
-        BytesMut {
-            inner: Inner::with_capacity_stable(capacity),
-        }
-    }
-
     /// Creates a new `BytesMut` with default capacity.
     ///
     /// Resulting object has length 0 and unspecified capacity.
@@ -1133,6 +1097,7 @@ impl BytesMut {
     /// use bytes::BytesMut;
     ///
     /// assert!(BytesMut::with_capacity(4).is_inline());
+    /// assert!(!BytesMut::from(Vec::with_capacity(4)).is_inline());
     /// assert!(!BytesMut::with_capacity(1024).is_inline());
     /// ```
     pub fn is_inline(&self) -> bool {
@@ -1593,6 +1558,11 @@ impl ops::DerefMut for BytesMut {
 }
 
 impl From<Vec<u8>> for BytesMut {
+    /// Convert a `Vec` into a `BytesMut`
+    ///
+    /// This constructor may be used to avoid the inlining optimization used by
+    /// `with_capacity`.  A `BytesMut` constructed this way will always store
+    /// its data on the heap.
     fn from(src: Vec<u8>) -> BytesMut {
         BytesMut {
             inner: Inner::from_vec(src),
@@ -1805,13 +1775,8 @@ impl Inner {
                 inner
             }
         } else {
-            Inner::with_capacity_stable(capacity)
-        }
-    }
-
-    #[inline]
-    fn with_capacity_stable(capacity:usize) -> Inner {
             Inner::from_vec(Vec::with_capacity(capacity))
+        }
     }
 
     /// Return a slice for the handle's view into the shared buffer
