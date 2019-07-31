79e4b2847f27137faaf149d116a352cbeae47fd1
nanpuyue <nanpuyue@gmail.com>
2019-07-31 08:05:51 +0800

    use `mem::MaybeUninit` instead of `mem::uninitialized`

--- src/bytes.rs
+++ src/bytes.rs
@@ -1666,7 +1666,7 @@ impl<'a> From<&'a [u8]> for BytesMut {
             BytesMut::new()
         } else if len <= INLINE_CAP {
             unsafe {
-                let mut inner: Inner = mem::uninitialized();
+                let mut inner: Inner = mem::MaybeUninit::uninit().assume_init();
 
                 // Set inline mask
                 inner.arc = AtomicPtr::new(KIND_INLINE as *mut Shared);
@@ -1858,7 +1858,7 @@ impl Inner {
         if capacity <= INLINE_CAP {
             unsafe {
                 // Using uninitialized memory is ~30% faster
-                let mut inner: Inner = mem::uninitialized();
+                let mut inner: Inner = mem::MaybeUninit::uninit().assume_init();
                 inner.arc = AtomicPtr::new(KIND_INLINE as *mut Shared);
                 inner
             }
@@ -2176,7 +2176,7 @@ impl Inner {
 
         if self.is_inline_or_static() {
             // In this case, a shallow_clone still involves copying the data.
-            let mut inner: Inner = mem::uninitialized();
+            let mut inner: Inner = mem::MaybeUninit::uninit().assume_init();
             ptr::copy_nonoverlapping(
                 self,
                 &mut inner,
