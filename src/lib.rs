2ed9c7949f00c48ef2ef03651a8e35f9334f5861
Steven Fackler <sfackler@palantir.com>
2019-11-25 14:49:29 -0500

    Prepare v0.5.1 release

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(warnings, missing_docs, missing_debug_implementations, rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/bytes/0.5.0")]
+#![doc(html_root_url = "https://docs.rs/bytes/0.5.1")]
 #![no_std]
 
 //! Provides abstractions for working with bytes.
