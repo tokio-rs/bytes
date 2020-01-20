ad17bc585337e275644522a5d6940fa3fce147a2
Sean McArthur <sean@seanmonstar.com>
2020-01-20 12:04:38 -0800

    Fix Bytes::truncate losing the original Vec's capacity

--- src/bytes.rs
+++ src/bytes.rs
@@ -418,7 +418,15 @@ impl Bytes {
     #[inline]
     pub fn truncate(&mut self, len: usize) {
         if len < self.len {
-            self.len = len;
+            // The Vec "promotable" vtables do not store the capacity,
+            // so we cannot truncate while using this repr. We *have* to
+            // promote using `split_off` so the capacity can be stored.
+            if self.vtable as *const Vtable == &PROMOTABLE_EVEN_VTABLE ||
+                self.vtable as *const Vtable == &PROMOTABLE_ODD_VTABLE {
+                drop(self.split_off(len));
+            } else {
+                self.len = len;
+            }
         }
     }
 
