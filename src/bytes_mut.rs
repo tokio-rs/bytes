817b76e6236e39b058b94366c15846e5013b4ad2
Tomasz Miąsko <tomasz.miasko@gmail.com>
2020-07-02 00:00:00 +0000

    Remove unnecessary synchronization when cloning shared representation

--- src/bytes_mut.rs
+++ src/bytes_mut.rs
@@ -1495,7 +1495,7 @@ static SHARED_VTABLE: Vtable = Vtable {
 };
 
 unsafe fn shared_v_clone(data: &AtomicPtr<()>, ptr: *const u8, len: usize) -> Bytes {
-    let shared = data.load(Ordering::Acquire) as *mut Shared;
+    let shared = data.load(Ordering::Relaxed) as *mut Shared;
     increment_shared(shared);
 
     let data = AtomicPtr::new(shared as _);
