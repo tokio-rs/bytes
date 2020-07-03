3603cec7c238a3414058e76d08ce9daa4a09d61c
Juan Aguilar <mhpoin@gmail.com>
2020-07-03 23:47:32 +0000

    Add inline attribute to BytesMut::set_len (#408)

--- src/bytes_mut.rs
+++ src/bytes_mut.rs
@@ -475,6 +475,7 @@ impl BytesMut {
     ///
     /// assert_eq!(&b[..], b"hello world");
     /// ```
+    #[inline]
     pub unsafe fn set_len(&mut self, len: usize) {
         debug_assert!(len <= self.cap, "set_len out of bounds");
         self.len = len;
