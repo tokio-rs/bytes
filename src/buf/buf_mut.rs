d91db02fd94b4801191f429871486f90e4f5656d
Alice Ryhl <alice@ryhl.io>
2021-08-07 07:37:42 +0200

    fix tests

--- src/buf/buf_mut.rs
+++ src/buf/buf_mut.rs
@@ -1033,7 +1033,7 @@ unsafe impl BufMut for Vec<u8> {
     #[inline]
     fn remaining_mut(&self) -> usize {
         // A vector can never have more than isize::MAX bytes
-        isize::MAX as usize - self.len()
+        std::isize::MAX as usize - self.len()
     }
 
     #[inline]
