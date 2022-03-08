8cc40be88994215a693aefdcb56cfa35bba38edd
Ralf Jung <post@ralfj.de>
2022-03-08 15:54:51 -0500

    update Miri CI config

--- ci/miri.sh
+++ ci/miri.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/miri)
-echo "Installing latest nightly with Miri: $MIRI_NIGHTLY"
-rustup set profile minimal
-rustup default "$MIRI_NIGHTLY"
-rustup component add miri
+rustup toolchain install nightly --component miri
+rustup override set nightly
+cargo miri setup
 
 cargo miri test
 cargo miri test --target mips64-unknown-linux-gnuabi64
