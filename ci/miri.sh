666632becd953142d1e0a303e1fd51671c4dff01
Alice Ryhl <aliceryhl@google.com>
2022-04-06 16:08:31 +0000

    Make strict provenance compatible

--- ci/miri.sh
+++ ci/miri.sh
@@ -5,5 +5,7 @@ rustup toolchain install nightly --component miri
 rustup override set nightly
 cargo miri setup
 
+export MIRIFLAGS="-Zmiri-strict-provenance"
+
 cargo miri test
 cargo miri test --target mips64-unknown-linux-gnuabi64
