ff47582e872710656283251b4f365c74ee552bb6
Jiahao XU <Jiahao_XU@outlook.com>
2022-07-12 00:19:59 +1000

    Use `slice::to_vec` explicitly in `static_to_vec`
    Co-authored-by: Alice Ryhl <aliceryhl@google.com>

--- src/bytes.rs
+++ src/bytes.rs
@@ -882,7 +882,7 @@ unsafe fn static_clone(_: &AtomicPtr<()>, ptr: *const u8, len: usize) -> Bytes {
 
 unsafe fn static_to_vec(_: &AtomicPtr<()>, ptr: *const u8, len: usize) -> Vec<u8> {
     let slice = slice::from_raw_parts(ptr, len);
-    slice.into()
+    slice.to_vec()
 }
 
 unsafe fn static_drop(_: &mut AtomicPtr<()>, _: *const u8, _: usize) {
