637d0c198476820b0185e7e9597335a43b4a494c
Taiki Endo <te316e89@gmail.com>
2024-01-07 23:35:53 +0900

    ci: Use install-action to install cargo-hack and cross

--- ci/test-stable.sh
+++ ci/test-stable.sh
@@ -4,10 +4,6 @@ set -ex
 
 cmd="${1:-test}"
 
-# Install cargo-hack for feature flag test
-host=$(rustc -Vv | grep host | sed 's/host: //')
-curl -LsSf https://github.com/taiki-e/cargo-hack/releases/latest/download/cargo-hack-$host.tar.gz | tar xzf - -C ~/.cargo/bin
-
 # Run with each feature
 # * --each-feature includes both default/no-default features
 # * --optional-deps is needed for serde feature
