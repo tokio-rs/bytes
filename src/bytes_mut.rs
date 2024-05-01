3a0e2a4b0b4d81bcd2b650776da7fb8433cf9b09
Emile Fugulin <code@efugulin.com>
2024-05-01 14:33:44 -0400

    Improve BytesMut SHARED capacity computing

--- src/bytes_mut.rs
+++ src/bytes_mut.rs
@@ -1761,8 +1761,15 @@ unsafe fn shared_v_to_mut(data: &AtomicPtr<()>, ptr: *const u8, len: usize) -> B
     if (*shared).is_unique() {
         let shared = &mut *shared;
 
+        // The capacity of the buffer is always the original capacity
+        // of the buffer minus the offset from the start of that buffer.
+        let v = &mut shared.vec;
+        let v_capacity = v.capacity();
+        let v_ptr = v.as_mut_ptr();
+        let offset = offset_from(ptr as *mut u8, v_ptr);
+        let cap = v_capacity.checked_sub(offset).unwrap_or(len);
+
         let ptr = vptr(ptr as *mut u8);
-        let cap = len; // It will try to reclaim the buffer on first insert
 
         BytesMut {
             ptr,
