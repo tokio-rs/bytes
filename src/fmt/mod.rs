bd4525250ef73bd52a8a7b8040c58bf4b34b05d5
Leonid Logvinov <logvinov.leon@gmail.com>
2024-10-26 11:59:06 +0200

    Rename hex_impl to fmt_impl and reuse it for fmt::Debug

--- src/fmt/mod.rs
+++ src/fmt/mod.rs
@@ -1,4 +1,16 @@
+macro_rules! fmt_impl {
+    ($tr:ident, $ty:ty) => {
+        impl $tr for $ty {
+            fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+                $tr::fmt(&BytesRef(self.as_ref()), f)
+            }
+        }
+    };
+}
+
+#[macro_use]
 mod debug;
+#[macro_use]
 mod hex;
 
 /// `BytesRef` is not a part of public API of bytes crate.
