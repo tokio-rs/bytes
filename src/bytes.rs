e10cfb68d6b57eb336073d35fd8a00cf9e1d6399
Adam Cimarosti <amunra@users.noreply.github.com>
2024-10-28 12:58:29 +0000

    atomic ordering fix
    Co-authored-by: Alice Ryhl <aliceryhl@google.com>

--- src/bytes.rs
+++ src/bytes.rs
@@ -1168,10 +1168,11 @@ unsafe fn owned_drop_impl(owned: *mut ()) {
     let lifetime = owned.cast::<OwnedLifetime>();
     let ref_cnt = &(*lifetime).ref_cnt;
 
-    let old_cnt = ref_cnt.fetch_sub(1, Ordering::Acquire);
+    let old_cnt = ref_cnt.fetch_sub(1, Ordering::Release);
     if old_cnt != 1 {
         return;
     }
+    ref_cnt.load(Ordering::Acquire);
 
     let drop_fn = &(*lifetime).drop;
     drop_fn(owned)
