7c838998382313cd8a41c59cf75dd38dcbb6bba5
Alice Ryhl <aliceryhl@google.com>
2024-12-10 17:02:26 +0100

    tmp restrict test

--- ci/panic-abort.sh
+++ ci/panic-abort.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -ex
-RUSTFLAGS="$RUSTFLAGS -Cpanic=abort -Zpanic-abort-tests" cargo test --all-features --test '*' -- --nocapture
+RUSTFLAGS="$RUSTFLAGS -Cpanic=abort -Zpanic-abort-tests" cargo test --all-features --test test_bytes_vec_alloc -- --nocapture
