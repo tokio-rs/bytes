d548dd080bcbd80715af06ef06f3bd81cb5dad1c
venomousmoog <venomousmoog@users.noreply.github.com>
2025-03-04 10:31:44 -0800

    underflow/overflow assertion should be <=

--- src/bytes.rs
+++ src/bytes.rs
@@ -1162,7 +1162,7 @@ unsafe fn owned_drop_impl(owned: *mut ()) {
 
     let old_cnt = ref_cnt.fetch_sub(1, Ordering::Release);
     debug_assert!(
-        old_cnt > 0 && old_cnt < usize::MAX >> 1,
+        old_cnt > 0 && old_cnt <= usize::MAX >> 1,
         "expected non-zero refcount and no underflow"
     );
     if old_cnt != 1 {
