25a3798622b5d7601e7043a4a603c2f65ed30765
ci <ci>
2025-11-28 09:19:02 +0000

    Deploy Bytes API documentation

---
+++ trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bytes",[["impl&lt;T: <a class=\"trait\" href=\"bytes/buf/trait.Buf.html\" title=\"trait bytes::buf::Buf\">Buf</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"bytes/buf/struct.IntoIter.html\" title=\"struct bytes::buf::IntoIter\">IntoIter</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[423]}
