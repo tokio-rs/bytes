428041f08ab7b431098b0318d991a2bae191e34f
ci <ci>
2026-02-03 15:02:38 +0000

    Deploy Bytes API documentation

---
+++ trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bytes",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"bytes/struct.TryGetError.html\" title=\"struct bytes::TryGetError\">TryGetError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[272]}
---
+++ trait.impl/core/fmt/trait.Write.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bytes",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"bytes/struct.BytesMut.html\" title=\"struct bytes::BytesMut\">BytesMut</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[257]}
